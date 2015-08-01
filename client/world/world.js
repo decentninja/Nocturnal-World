e84e34d43128c88aa8953216a8ae2e66af9468b1
decentninja <very@decent.ninja>
2015-08-01 14:30:21 +0200

    Initial state sync, no update yet

--- client/world/world.js
+++ client/world/world.js
@@ -14,6 +14,15 @@
 		}
 	};
 	var slices = {};
+	function map_update(data) {
+		Object.keys(data).forEach(function(x) {
+			if(!map[x])
+				map[x] = {};
+			Object.keys(data[x]).forEach(function(y) {
+				map[x][y] = data[x][y];
+			});
+		});
+	}
 	var world = {
 		getTile: function(x, y) {
 			var x_slice = Math.floor(x / SLICE_SIZE);
@@ -24,7 +33,21 @@
 				var url = 'world slice ' + x_slice + ' ' + y_slice;
 				url = url.replace(/\-/g, 'c');
 				var slice = nocturnal.create(url, function(question) {
-					// TODO send initial state
+					// send initial state
+					switch(question.action) {
+						case 'complete_state':
+							console.log('got request for slice', x_slice, y_slice);
+							var mapslice = {};
+							for(var x = x_slice * SLICE_SIZE; x < (x_slice + 1) * SLICE_SIZE; x++) {
+								if(!mapslice[x])
+									mapslice[x] = {};
+								for(var y = y_slice * SLICE_SIZE; y < (y_slice + 1) * SLICE_SIZE; y++) {
+									if(mapslice[x] && mapslice[x][y])
+										mapslice[x][y] = map[x][y];
+								}
+							}
+							return mapslice;
+					}
 				});
 				var nodup = true;
 				slice.addEventListener('error', function(error) {
@@ -35,12 +58,16 @@
 						console.log('slice', x_slice, y_slice, 'is already occupided, tries to connect');
 						delete slice;
 						var slice = nocturnal.subscribe(url, function(data) {
-							// TODO update map with new data
+							console.log('got broadcast data for slice', x_slice, y_slice);
+							map_update(data);
 						});
 						slice.addEventListener('open', function() {
 							console.log('slice', x_slice, y_slice, 'connected and listening to');
 							slices[x_slice][y_slice] = slice;
-							// TODO request initial state
+							slice.request({action: 'complete_state'}, function(data) {
+								console.log('got complete_state answer for slice', x_slice, y_slice);
+								map_update(data);
+							});
 						});
 					}
 					return false;
@@ -54,7 +81,7 @@
 			else
 				return 'white';
 		},
-		SLICE_SIZE: SLICE_SIZE
+		SLICE_SIZE: SLICE_SIZE,
 	};
 	window.world = world;
 })();
